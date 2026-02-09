bfa5341bd6509a275874f6be175bf2c9f2d1200f
cyberpwn <dan@arcane.art>
2026-02-08 22:29:41 -0600

    f

--- src/main/java/art/arcane/satiscraftory/data/ConveyorTier.java
+++ src/main/java/art/arcane/satiscraftory/data/ConveyorTier.java
@@ -4,9 +4,9 @@ public enum ConveyorTier {
     MARK_1(60),
     MARK_2(30),
     MARK_3(15),
-    MARK_4(7),
-    MARK_5(3),
-    MARK_6(1);
+    MARK_4(8),
+    MARK_5(4),
+    MARK_6(2);
     
     private final int speed;
     
